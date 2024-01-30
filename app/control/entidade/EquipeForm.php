2f65bd73a07122eb95bc0f0e48861f397082159b
babbujah <bczion@hotmail.com>
2024-01-30 17:36:03 -0300

    Inclusão das listagens e formulários de cadastro de equipes.

---
+++ app/control/entidade/EquipeForm.php
@@ -0,0 +1,156 @@
+<?php
+/**
+* Classe que representa o formulário de cadastro de uma equipe.
+* EquipeForm Registration
+*
+* @version    1.0
+* @package    control/entidade
+* @author     Bruno Lopes
+* @since      30/01/2024
+**/
+class EquipeForm extends TPage
+{
+    protected $form; // form
+    
+    /**
+     * Form constructor
+     * @param $param Request
+     */
+    public function __construct( $param )
+    {
+        parent::__construct();
+        
+        
+        // creates the form
+        $this->form = new BootstrapFormBuilder('form_Equipe');
+        $this->form->setFormTitle('Matilha/Patrulha/Equipe');
+        
+
+        // create the form fields
+        $id = new TEntry('id');
+        $id->setSize('100%');
+        
+        $nome = new TEntry('nome');
+        $nome->setSize('100%');
+        
+        $tipo = new TCombo('tipo');
+        $tipo->addItems(Equipe::TIPOEQUIPE);
+        $tipo->setSize('100%');
+        
+        //$data_criacao = new TDate('data_criacao');
+
+
+        // add the fields
+        $this->form->addFields( [ new TLabel('Id') ], [ $id ] );
+        $this->form->addFields( [ new TLabel('Nome') ], [ $nome ] );
+        $this->form->addFields( [ new TLabel('Tipo') ], [ $tipo ] );
+        //$this->form->addFields( [ new TLabel('Data Criacao') ], [ $data_criacao ] );
+
+
+
+        // set sizes
+        //$data_criacao->setSize('100%');
+
+
+
+        if (!empty($id))
+        {
+            $id->setEditable(FALSE);
+        }
+        
+        /** samples
+         $fieldX->addValidation( 'Field X', new TRequiredValidator ); // add validation
+         $fieldX->setSize( '100%' ); // set size
+         **/
+         
+        // create the form actions
+        $btn = $this->form->addAction(_t('Save'), new TAction([$this, 'onSave']), 'fa:save');
+        $btn->class = 'btn btn-sm btn-primary';
+        $this->form->addActionLink(_t('New'),  new TAction([$this, 'onEdit']), 'fa:eraser red');
+        $this->form->addActionLink(_t('Back'),  new TAction(['EquipeList', 'onReload']), 'fa:undo');
+        
+        // vertical box container
+        $container = new TVBox;
+        $container->style = 'width: 100%';
+        // $container->add(new TXMLBreadCrumb('menu.xml', __CLASS__));
+        $container->add($this->form);
+        
+        parent::add($container);
+    }
+
+    /**
+     * Save form data
+     * @param $param Request
+     */
+    public function onSave( $param )
+    {
+        try
+        {
+            TTransaction::open('siuel'); // open a transaction
+            
+            /**
+            // Enable Debug logger for SQL operations inside the transaction
+            TTransaction::setLogger(new TLoggerSTD); // standard output
+            TTransaction::setLogger(new TLoggerTXT('log.txt')); // file
+            **/
+            
+            $this->form->validate(); // validate form data
+            $data = $this->form->getData(); // get form data as array
+            
+            $object = new Equipe;  // create an empty object
+            $object->fromArray( (array) $data); // load the object with data
+            $object->store(); // save the object
+            
+            // get the generated id
+            $data->id = $object->id;
+            
+            $this->form->setData($data); // fill form data
+            TTransaction::close(); // close the transaction
+            
+            new TMessage('info', AdiantiCoreTranslator::translate('Record saved'));
+        }
+        catch (Exception $e) // in case of exception
+        {
+            new TMessage('error', $e->getMessage()); // shows the exception error message
+            $this->form->setData( $this->form->getData() ); // keep form data
+            TTransaction::rollback(); // undo all pending operations
+        }
+    }
+    
+    /**
+     * Clear form data
+     * @param $param Request
+     */
+    public function onClear( $param )
+    {
+        $this->form->clear(TRUE);
+    }
+    
+    /**
+     * Load object to form data
+     * @param $param Request
+     */
+    public function onEdit( $param )
+    {
+        try
+        {
+            if (isset($param['key']))
+            {
+                $key = $param['key'];  // get the parameter $key
+                TTransaction::open('siuel'); // open a transaction
+                $object = new Equipe($key); // instantiates the Active Record
+                $this->form->setData($object); // fill the form
+                TTransaction::close(); // close the transaction
+            }
+            else
+            {
+                $this->form->clear(TRUE);
+            }
+        }
+        catch (Exception $e) // in case of exception
+        {
+            new TMessage('error', $e->getMessage()); // shows the exception error message
+            TTransaction::rollback(); // undo all pending operations
+        }
+    }
+}
